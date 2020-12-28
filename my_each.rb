a6e994e2f1a0a9e575f63acfaa85458453ab46ad
Luis Martinez <luis_mmartinez@live.com>
2020-12-28 04:44:32 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,18 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
+  if block_given?
+    i=0
+
+    while i < array.length
+      yield (array[i])
+      i= i+1
+    end
+    array
+  else
+    "This block should not run!"
+  end
 end
+
+#my_each(array) do |i|
+#  puts i
+#end
